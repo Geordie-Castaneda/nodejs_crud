b9c9c368c95acedd8dae6ad94ff45fdecd59e2f0
Geordie Castaneda <jordyorantes9@gmail.com>
2023-08-19 20:59:50 -0600

    Clase 2 Node.js: Conexion a Mysql

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { conectar } from "./modelo/db_conectar.js";
 const app_e = express();
+app_e.use(express.static('./vista'))
+app_e.use(express.static('./controlador'))
+app_e.use(express.static('./modelo'))
+
+
 app_e.listen('5000',function(){
     console.log('Aplicacion Iniciada : http://localhost:5000/')
 })
-
 app_e.get('/',function(req,res){
-    res.send("Hola mundo, primeros cambios")
+    res.send("Hola mundo...")
 })
 
