68b5312663881a2cf9b1c549910b31f3d73e02c9
Geordie Castaneda <jordyorantes9@gmail.com>
2023-08-20 13:03:43 -0600

    Clase 4 Node.js: CRUD Node.js Crear, Actualizar y Borrar

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import { crud_cliente } from "./controlador/crud_clientes.js";
 
 const app_e = express();
+
+app_e.use(express.urlencoded({extended:false}));
+app_e.use(express.json());
 app_e.use(express.static('./vista'))
 app_e.use(express.static('./controlador'))
 app_e.use(express.static('./modelo'))
@@ -12,3 +15,4 @@ app_e.listen('5000',function(){
     console.log('Aplicacion Iniciada : http://localhost:5000/')
 })
 app_e.get('/',crud_cliente.leer);
+app_e.post('/crud_c', crud_cliente.cud);
